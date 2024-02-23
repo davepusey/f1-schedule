c24d778c67f57031ec53028c54c3c938c6ad68b4
Dave Pusey <dave@puseyuk.co.uk>
2024-02-23 16:59:53 +0000

    2024 01 BAHRAIN

--- data.js
+++ data.js
@@ -70,6 +70,15 @@ var roundFlagABU = "https://upload.wikimedia.org/wikipedia/commons/c/cb/Flag_of_
 var roundNameQAT = "Qatar Grand Prix";
 var roundFlagQAT = "https://upload.wikimedia.org/wikipedia/commons/6/65/Flag_of_Qatar.svg";
 
+var roundNameCHI = "Chinese Grand Prix";
+var roundFlagCHI = "https://upload.wikimedia.org/wikipedia/commons/f/fa/Flag_of_the_People%27s_Republic_of_China.svg";
+
+var roundNameRUS = "Russian Grand Prix";
+var roundFlagRUS = "https://upload.wikimedia.org/wikipedia/en/f/f3/Flag_of_Russia.svg";
+
+var roundNameVIE = "Vietnamese Grand Prix";
+var roundFlagVIE = "https://upload.wikimedia.org/wikipedia/commons/2/21/Flag_of_Vietnam.svg";
+
 var roundNameMIA = "Miami Grand Prix";
 var roundFlagMIA = roundFlagUSA;
 
@@ -78,464 +87,48 @@ var roundFlagLAS = roundFlagUSA;
 
 //----------------------------------------------------------------------------------------------------------------------
 
-var seasonYear = 2023;
+var seasonYear = 2024;
 
 var roundNum = 0;
 var roundName = "Bahrain Test";
 var roundFlag = roundFlagBAH;
 
 var sessionsData = [
-	["Day 1 AM", new Date(1677135600 * SECS), 255 * MINS],
-	["Day 1 PM", new Date((1677153600 * SECS) + (15 * MINS)), 255 * MINS],
 
-	["Day 2 AM", new Date(1677222000 * SECS), 255 * MINS],
-	["Day 2 PM", new Date((1677240000 * SECS) + (15 * MINS)), 255 * MINS],
+	// 2024-02-21 WED
+	["Day 1 AM",       new Date("2024-02-21T07:00:00Z"), 240 * MINS],
+	["Day 1 PM",       new Date("2024-02-21T12:00:00Z"), 240 * MINS],
 
-	["Day 3 AM", new Date(1677308400 * SECS), 255 * MINS],
-	["Day 3 PM", new Date((1677326400 * SECS) + (15 * MINS)), 255 * MINS]
+	// 2024-02-22 THU
+	["Day 2 AM",       new Date("2024-02-22T07:00:00Z"), 180 * MINS],
+	["Day 2 PM",       new Date("2024-02-22T11:00:00Z"), 310 * MINS],
+
+	// 2024-02-23 FRI
+	["Day 3",          new Date("2024-02-23T07:00:00Z"), 550 * MINS]
 ];
 
-roundNum = 1;
+roundNum++;
 roundName = roundNameBAH;
 roundFlag = roundFlagBAH;
 
-var sessionsData = [
-	["F3 Practice", new Date(1677830100 * SECS), 45 * MINS],
-	["F2 Practice", new Date(1677834300 * SECS), 45 * MINS],
-	["F1 Practice 1", new Date(1677843000 * SECS), 60 * MINS],
-	["F3 Qualifying", new Date(1677848400 * SECS), 30 * MINS],
-	["F1 Practice 2", new Date(1677855600 * SECS), 60 * MINS],
-	["F2 Qualifying", new Date(1677861000 * SECS), 30 * MINS],
-
-	["F3 Race 1", new Date(1677921300 * SECS), 45 * MINS],
-	["F1 Practice 3", new Date(1677929400 * SECS), 60 * MINS],
-	["F2 Race 1", new Date(1677935700 * SECS), 45 * MINS],
-	["F1 Qualifying", new Date(1677942000 * SECS), 60 * MINS],
-
-	["F3 Race 2", new Date(1678006200 * SECS), 45 * MINS],
-	["F2 Race 2", new Date(1678011600 * SECS), 60 * MINS],
-	["F1 Race", new Date(1678028400 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameSAU;
-roundFlag = roundFlagSAU;
-
-var sessionsData = [
-	["F2 Practice", new Date(1679050500 * SECS), 45 * MINS],
-	["F1 Practice 1", new Date(1679059800 * SECS), 60 * MINS],
-	["F2 Qualifying", new Date(1679065200 * SECS), 30 * MINS],
-	["F1 Practice 2", new Date(1679072400 * SECS), 60 * MINS],
-
-	["F1 Practice 3", new Date(1679146200 * SECS), 60 * MINS],
-	["F2 Race 1", new Date(1679152200 * SECS), 45 * MINS],
-	["F1 Qualifying", new Date(1679158800 * SECS), 60 * MINS],
-
-	["F2 Race 2", new Date(1679231700 * SECS), 60 * MINS],
-	["F1 Race", new Date(1679245200 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameAUS;
-roundFlag = roundFlagAUS;
-
-var sessionsData = [
-	["SCars Race 1", new Date(1680158400 * SECS), 45 * MINS],
-
-	["F3 Practice", new Date(1680213000 * SECS), 45 * MINS],
-	["F2 Practice", new Date(1680217200 * SECS), 45 * MINS],
-	["F1 Practice 1", new Date(1680226200 * SECS), 60 * MINS],
-	["F3 Qualifying", new Date(1680231600 * SECS), 30 * MINS],
-	["SCars Race 2", new Date(1680234600 * SECS), 35 * MINS],
-	["F1 Practice 2", new Date(1680238800 * SECS), 60 * MINS],
-	["F2 Qualifying", new Date(1680244200 * SECS), 30 * MINS],
-
-	["F3 Race 1", new Date(1680306300 * SECS), 40 * MINS],
-	["F1 Practice 3", new Date(1680312600 * SECS), 60 * MINS],
-	["F2 Race 1", new Date(1680319200 * SECS), 45 * MINS],
-	["F1 Qualifying", new Date(1680325200 * SECS), 60 * MINS],
-	["SCars Race 3", new Date(1680330300 * SECS), 30 * MINS],
-
-	["F3 Race 2", new Date(1680390300 * SECS), 45 * MINS],
-	["SCars Race 4", new Date(1680394800 * SECS), 30 * MINS],
-	["F2 Race 2", new Date(1680399300 * SECS), 60 * MINS],
-	["F1 Race", new Date(1680411600 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameAZE;
-roundFlag = roundFlagAZE;
-
-var sessionsData = [
-	["F2 Practice", new Date(1682665500 * SECS), 45 * MINS],
-	["F1 Practice 1", new Date(1682674200 * SECS), 60 * MINS],
-	["F2 Qualifying", new Date(1682679600 * SECS), 30 * MINS],
-	["F1 Qualifying", new Date(1682686800 * SECS), 60 * MINS],
-
-	["F1 Sprint Qual", new Date(1682760600 * SECS), 60 * MINS],
-	["F2 Race 1", new Date(1682766900 * SECS), 45 * MINS],
-	["F1 Sprint", new Date(1682775000 * SECS), 90 * MINS],
-
-	["F2 Race 2", new Date(1682840100 * SECS), 60 * MINS],
-	["F1 Race", new Date(1682852400 * SECS), 180 * MINS]
-];
-
-roundNum = 5;
-roundName = roundNameMIA;
-roundFlag = roundFlagMIA;
-
-var sessionsData = [
-	["F1 Practice 1", new Date(1683311400 * SECS), 60 * MINS],
-	["F1 Practice 2", new Date(1683324000 * SECS), 60 * MINS],
-	["FE @ Monaco", new Date(1683378180 * SECS), 180 * MINS],
-	["F1 Practice 3", new Date(1683390600 * SECS), 60 * MINS],
-	["F1 Qualifying", new Date(1683403200 * SECS), 60 * MINS],
-	["F1 Race", new Date(1683487800 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameMON;
-roundFlag = roundFlagMON;
-
-var sessionsData = [
-	["F3 Practice", new Date(1685014200 * SECS), 45 * MINS],
-	["F2 Practice", new Date(1685019600 * SECS), 45 * MINS],
-	["PSC Practice", new Date(1685025000 * SECS), 45 * MINS],
-
-	["F3 Qual A", new Date(1685092200 * SECS), 16 * MINS],
-	["F3 Qual B", new Date(1685093640 * SECS), 16 * MINS],
-	["F1 Practice 1", new Date(1685100600 * SECS), 60 * MINS],
-	["F2 Qual A", new Date(1685106600 * SECS), 16 * MINS],
-	["F2 Qual B", new Date(1685108040 * SECS), 16 * MINS],
-	["F1 Practice 2", new Date(1685113200 * SECS), 60 * MINS],
-	["PSC Qual", new Date(1685119500 * SECS), 35 * MINS],
-
-	["F3 Race 1", new Date(1685178000 * SECS), 40 * MINS],
-	["F1 Practice 3", new Date(1685183400 * SECS), 60 * MINS],
-	["F2 Race 1", new Date(1685189700 * SECS), 45 * MINS],
-	["F1 Qualifying", new Date(1685196000 * SECS), 60 * MINS],
-
-	["F3 Race 2", new Date(1685253600 * SECS), 45 * MINS],
-	["F2 Race 2", new Date(1685259900 * SECS), 60 * MINS],
-	["PSC Race", new Date(1685267700 * SECS), 30 * MINS],
-	["F1 Race", new Date(1685278800 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameSPA;
-roundFlag = roundFlagSPA;
-
-var sessionsData = [
-	["F3 Practice", new Date(1685692500 * SECS), 16 * MINS],
-	["F2 Practice", new Date(1685696700 * SECS), 16 * MINS],
-	["F1 Practice 1", new Date(1685705400 * SECS), 60 * MINS],
-	["F3 Qualifying", new Date(1685710800 * SECS), 30 * MINS],
-	["F2 Qualifying", new Date(1685714100 * SECS), 30 * MINS],
-	["F1 Practice 2", new Date(1685718000 * SECS), 60 * MINS],
-
-	["F3 Race 1", new Date(1685781000 * SECS), 40 * MINS],
-	["F1 Practice 3", new Date(1685788200 * SECS), 60 * MINS],
-	["F2 Race 1", new Date(1685794500 * SECS), 45 * MINS],
-	["F1 Qualifying", new Date(1685800800 * SECS), 60 * MINS],
-
-	["F3 Race 2", new Date(1685865300 * SECS), 45 * MINS],
-	["F2 Race 2", new Date(1685870700 * SECS), 60 * MINS],
-	["F1 Race", new Date(1685883600 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameCAN;
-roundFlag = roundFlagCAN;
-
-var sessionsData = [
-	["F1 Practice 1", new Date(1686936600 * SECS), 60 * MINS],
-	["F1 Practice 2", new Date(1686947400 * SECS), 90 * MINS],
-
-	["F1 Practice 3", new Date(1687019400 * SECS), 60 * MINS],
-	["F1 Qualifying", new Date(1687032000 * SECS), 60 * MINS],
-
-	["F1 Race", new Date(1687111200 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameAUT;
-roundFlag = roundFlagAUT;
-
-var sessionsData = [
-	["F3 Practice", new Date(1688111700 * SECS), 45 * MINS],
-	["F2 Practice", new Date(1688115900 * SECS), 45 * MINS],
-	["F1 Practice 1", new Date(1688124600 * SECS), 60 * MINS],
-	["F3 Qualifying", new Date(1688130000 * SECS), 30 * MINS],
-	["F2 Qualifying", new Date(1688133300 * SECS), 30 * MINS],
-	["F1 Qualifying", new Date(1688137200 * SECS), 60 * MINS],
-	["PSC Practice", new Date(1688142600 * SECS), 45 * MINS],
-
-	["F3 Race 1", new Date(1688198400 * SECS), 40 * MINS],
-	["F1 Sprint Qual", new Date(1688205600 * SECS), 60 * MINS],
-	["F2 Race 1", new Date(1688211900 * SECS), 45 * MINS],
-	["PSC Qual", new Date(1688216400 * SECS), 30 * MINS],
-	["F1 Sprint", new Date(1688221800 * SECS), 90 * MINS],
-
-	["F3 Race 2", new Date(1688279100 * SECS), 45 * MINS],
-	["F2 Race 2", new Date(1688284500 * SECS), 60 * MINS],
-	["PSC Race", new Date(1688290800 * SECS), 30 * MINS],
-	["F1 Race", new Date(1688302800 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameGB;
-roundFlag = roundFlagGB;
-
-var sessionsData = [
-	["F3 Practice", new Date(1688715900 * SECS), 45 * MINS],
-	["F2 Practice", new Date(1688720700 * SECS), 45 * MINS],
-	["F1 Practice 1", new Date(1688729400 * SECS), 60 * MINS],
-	["F3 Qualifying", new Date(1688735400 * SECS), 30 * MINS],
-	["F2 Qualifying", new Date(1688738700 * SECS), 30 * MINS],
-	["F1 Practice 2", new Date(1688742000 * SECS), 60 * MINS],
-	["PSC Practice", new Date(1688748000 * SECS), 45 * MINS],
-
-	["F3 Race 1", new Date(1688804400 * SECS), 40 * MINS],
-	["F1 Practice 3", new Date(1688812200 * SECS), 60 * MINS],
-	["F2 Race 1", new Date(1688818500 * SECS), 45 * MINS],
-	["F1 Qualifying", new Date(1688824800 * SECS), 60 * MINS],
-	["PSC Qual", new Date(1688831100 * SECS), 30 * MINS],
-
-	["F3 Race 2", new Date(1688887200 * SECS), 45 * MINS],
-	["F2 Race 2", new Date(1688892900 * SECS), 60 * MINS],
-	["PSC Race", new Date(1688900100 * SECS), 30 * MINS],
-	["F1 Race", new Date(1688911200 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameHUN;
-roundFlag = roundFlagHUN;
-
-var sessionsData = [
-	["F3 Practice", new Date(1689926100 * SECS), 45 * MINS],
-	["F2 Practice", new Date(1689930300 * SECS), 45 * MINS],
-	["F1 Practice 1", new Date(1689939000 * SECS), 60 * MINS],
-	["F3 Qualifying", new Date(1689944700 * SECS), 30 * MINS],
-	["F2 Qualifying", new Date(1689948000 * SECS), 30 * MINS],
-	["F1 Practice 2", new Date(1689951600 * SECS), 60 * MINS],
-	["PSC Practice", new Date(1689957300 * SECS), 45 * MINS],
-
-	["F3 Race 1", new Date(1690012200 * SECS), 40 * MINS],
-	["PSC Qual", new Date(1690017600 * SECS), 30 * MINS],
-	["F1 Practice 3", new Date(1690021800 * SECS), 60 * MINS],
-	["F2 Race 1", new Date(1690028100 * SECS), 45 * MINS],
-	["F1 Qualifying", new Date(1690034400 * SECS), 60 * MINS],
-
-	["F3 Race 2", new Date(1690093500 * SECS), 45 * MINS],
-	["F2 Race 2", new Date(1690099500 * SECS), 60 * MINS],
-	["PSC Race", new Date(1690106400 * SECS), 30 * MINS],
-	["F1 Race", new Date(1690117200 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameBEL;
-roundFlag = roundFlagBEL;
-
-var sessionsData = [
-	["F3 Practice", new Date(1690530900 * SECS), 45 * MINS],
-	["F2 Practice", new Date(1690535100 * SECS), 45 * MINS],
-	["F1 Practice 1", new Date(1690543800 * SECS), 60 * MINS],
-	["F3 Qualifying", new Date(1690549200 * SECS), 30 * MINS],
-	["F2 Qualifying", new Date(1690552500 * SECS), 30 * MINS],
-	["F1 Qualifying", new Date(1690556400 * SECS), 60 * MINS],
-	["PSC Practice", new Date(1690561800 * SECS), 45 * MINS],
-
-	["F3 Race 1", new Date(1690617600 * SECS), 40 * MINS],
-	["F1 Sprint Qual", new Date((1690624800 * SECS) + (35 * MINS)), 60 * MINS],
-	["F2 Race 1", new Date(1690631100 * SECS), 45 * MINS],
-	["PSC Qual", new Date(1690635600 * SECS), 30 * MINS],
-	["F1 Sprint", new Date((1690641000 * SECS) + (35 * MINS)), 90 * MINS],
-
-	["F3 Race 2", new Date(1690698600 * SECS), 45 * MINS],
-	["F2 Race 2", new Date(1690704000 * SECS), 60 * MINS],
-	["PSC Race", new Date(1690710300 * SECS), 30 * MINS],
-	["F1 Race", new Date(1690722000 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameNET;
-roundFlag = roundFlagNET;
-
-var sessionsData = [
-	["F2 Practice", new Date(1692950700 * SECS), 45 * MINS],
-	["F1 Practice 1", new Date(1692959400 * SECS), 60 * MINS],
-	["F2 Qualifying", new Date(1692964800 * SECS), 30 * MINS],
-	["F1 Practice 2", new Date(1692972000 * SECS), 60 * MINS],
-	["PSC Practice", new Date(1692977400 * SECS), 45 * MINS],
-
-	["PSC Qual", new Date(1693036200 * SECS), 30 * MINS],
-	["F1 Practice 3", new Date(1693042200 * SECS), 60 * MINS],
-	["F2 Race 1", new Date(1693048500 * SECS), 45 * MINS],
-	["F1 Qualifying", new Date(1693054800 * SECS), 60 * MINS],
-	["PSC Race 1", new Date(1693062000 * SECS), 30 * MINS],
-
-	["F2 Race 2", new Date(1693123200 * SECS), 60 * MINS],
-	["PSC Race 2", new Date(1693130100 * SECS), 30 * MINS],
-	["F1 Race", new Date(1693141200 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameITA;
-roundFlag = roundFlagITA;
-
-var sessionsData = [
-	["F3 Practice", new Date(1693554000 * SECS), 45 * MINS],
-	["F2 Practice", new Date(1693559100 * SECS), 45 * MINS],
-	["F1 Practice 1", new Date(1693567800 * SECS), 60 * MINS],
-	["F3 Qualifying", new Date(1693573500 * SECS), 30 * MINS],
-	["F2 Qualifying", new Date(1693576800 * SECS), 30 * MINS],
-	["F1 Practice 2", new Date(1693580400 * SECS), 60 * MINS],
-	["PSC Practice", new Date(1693586100 * SECS), 45 * MINS],
-
-	["F3 Race 1", new Date(1693639500 * SECS), 40 * MINS],
-	["PSC Qual", new Date(1693644600 * SECS), 30 * MINS],
-	["F1 Practice 3", new Date(1693650600 * SECS), 60 * MINS],
-	["F2 Race 1", new Date(1693656900 * SECS), 45 * MINS],
-	["F1 Qualifying", new Date(1693663200 * SECS), 60 * MINS],
-
-	["F3 Race 2", new Date(1693721700 * SECS), 45 * MINS],
-	["F2 Race 2", new Date(1693727700 * SECS), 60 * MINS],
-	["PSC Race", new Date(1693734900 * SECS), 30 * MINS],
-	["F1 Race", new Date(1693746000 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameSIN;
-roundFlag = roundFlagSIN;
-
-var sessionsData = [
-	["F1 Practice 1", new Date(1694770200 * SECS), 60 * MINS],
-	["F1 Practice 2", new Date(1694782800 * SECS), 60 * MINS],
-
-	["F1 Practice 3", new Date(1694856600 * SECS), 60 * MINS],
-	["F1 Qualifying", new Date(1694869200 * SECS), 60 * MINS],
-
-	["F1 Race", new Date(1694952000 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameJAP;
-roundFlag = roundFlagJAP;
-
-var sessionsData = [
-	["F1 Practice 1", new Date(1695349800 * SECS), 60 * MINS],
-	["F1 Practice 2", new Date(1695362400 * SECS), 60 * MINS],
-
-	["F1 Practice 3", new Date(1695436200 * SECS), 60 * MINS],
-	["F1 Qualifying", new Date(1695448800 * SECS), 60 * MINS],
-
-	["F1 Race", new Date(1695531600 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameQAT;
-roundFlag = roundFlagQAT;
-
-var sessionsData = [
-	["F1 Practice 1", new Date(1696599000 * SECS), 60 * MINS],
-	["F1 Qualifying", new Date(1696611600 * SECS), 60 * MINS],
-
-	["F1 Track Familiarisation", new Date(1696683600 * SECS), 10 * MINS],
-	["F1 Sprint Qual", new Date(1696684800 * SECS), 60 * MINS],
-	["F1 Sprint", new Date(1696699800 * SECS), 90 * MINS],
-
-	["F1 Race", new Date(1696784400 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameUSA;
-roundFlag = roundFlagUSA;
-
-var sessionsData = [
-	["Academy P1", new Date(1697807700 * SECS), 40 * MINS],
-	["F1 Practice 1", new Date(1697823000 * SECS), 60 * MINS],
-	["Academy P2", new Date(1697828100 * SECS), 40 * MINS],
-	["F1 Qualifying", new Date(1697835600 * SECS), 60 * MINS],
-	["Academy Q1", new Date(1697841000 * SECS), 15 * MINS],
-	["Academy Q2", new Date(1697842500 * SECS), 15 * MINS],
-
-	["Academy R1", new Date(1697899500 * SECS), 30 * MINS],
-	["F1 Sprint Qual", new Date(1697909400 * SECS), 60 * MINS],
-	["Academy R2", new Date(1697920200 * SECS), 20 * MINS],
-	["F1 Sprint", new Date(1697925600 * SECS), 90 * MINS],
-
-	["Academy R3", new Date(1697985600 * SECS), 30 * MINS],
-	["F1 Race", new Date(1698001200 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameMEX;
-roundFlag = roundFlagMEX;
-
-var sessionsData = [
-	["F1 Practice 1", new Date(1698431400 * SECS), 60 * MINS],
-	["F1 Practice 2", new Date(1698444000 * SECS), 60 * MINS],
-
-	["F1 Practice 3", new Date(1698514200 * SECS), 60 * MINS],
-	["F1 Qualifying", new Date(1698526800 * SECS), 60 * MINS],
-
-	["F1 Race", new Date(1698609600 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameBRA;
-roundFlag = roundFlagBRA;
-
-var sessionsData = [
-	["F1 Practice 1", new Date(1699021800 * SECS), 60 * MINS],
-	["F1 Qualifying", new Date(1699034400 * SECS), 60 * MINS],
-
-	["F1 Sprint Qual", new Date(1699106400 * SECS), 60 * MINS],
-	["F1 Sprint", new Date(1699122600 * SECS), 90 * MINS],
-
-	["F1 Race", new Date(1699203600 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameLAS;
-roundFlag = roundFlagLAS;
-
-var sessionsData = [
-
-	// 2023-11-15 WED 
-	["Opening Ceremony", new Date(1700105400 * SECS), 0 * MINS],
-
-	// 2023-11-16 THU 
-	["F1 Practice 1", new Date(1700195400 * SECS), 60 * MINS],
-
-	// 2023-11-17 FRI
-	["F1 Practice 2", new Date(1700217000 * SECS), 90 * MINS],
-	["F1 Practice 3", new Date(1700281800 * SECS), 60 * MINS],
-
-	// 2023-11-18 SAT
-	["F1 Qualifying", new Date(1700294400 * SECS), 60 * MINS],
-	["Pre-Race Show (Grid)", new Date(1700368200 * SECS), 30 * MINS],
-	["F1 Race", new Date(1700373600 * SECS), 180 * MINS]
-];
-
-roundNum++;
-roundName = roundNameABU;
-roundFlag = roundFlagABU;
-
-var sessionsData = [
-
-	// 2023-11-24 FRI
-	["F2 Practice", new Date(1700809500 * SECS), 45 * MINS],
-	["F1 Practice 1", new Date(1700818200 * SECS), 60 * MINS],
-	["F2 Qualifying", new Date(1700823600 * SECS), 30 * MINS],
-	["F1 Practice 2", new Date(1700830800 * SECS), 60 * MINS],
-
-	// 2023-11-25 SAT
-	["F1 Practice 3", new Date(1700908200 * SECS), 60 * MINS],
-	["F2 Race 1", new Date(1700914800 * SECS), 45 * MINS],
-	["F1 Qualifying", new Date(1700920800 * SECS), 60 * MINS],
-
-	// 2023-11-26 SUN
-	["F2 Race 2", new Date(1700990100 * SECS), 60 * MINS],
-	["F1 Race", new Date(1701003600 * SECS), 180 * MINS]
+sessionsData = [
+
+	// 2024-02-29 THU
+	["F3 Practice",    new Date("2024-02-29T07:55:00Z"),  45 * MINS],
+	["F2 Practice",    new Date("2024-02-29T09:05:00Z"),  45 * MINS],
+	["F1 Practice 1",  new Date("2024-02-29T11:30:00Z"),  60 * MINS],
+	["F3 Qualifying",  new Date("2024-02-29T13:00:00Z"),  30 * MINS],
+	["F2 Qualifying",  new Date("2024-02-29T13:55:00Z"),  30 * MINS],
+	["F1 Practice 2",  new Date("2024-02-29T15:00:00Z"),  60 * MINS],
+
+	// 2024-03-01 FRI
+	["F3 Race 1",      new Date("2024-03-01T10:15:00Z"),  40 * MINS],
+	["F1 Practice 3",  new Date("2024-03-01T12:30:00Z"),  60 * MINS],
+	["F2 Race 1",      new Date("2024-03-01T14:15:00Z"),  45 * MINS],
+	["F1 Qualifying",  new Date("2024-03-01T16:00:00Z"),  60 * MINS],
+
+	// 2024-03-02 SAT
+	["F3 Race 2",      new Date("2024-03-02T09:00:00Z"),  45 * MINS],
+	["F2 Race 2",      new Date("2024-03-02T10:30:00Z"),  60 * MINS],
+	["F1 Race",        new Date("2024-03-02T15:00:00Z"), 180 * MINS]
 ];
