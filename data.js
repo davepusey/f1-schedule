c7899d0a349f69afe12d8c9043e2d59458246a07
Dave Pusey <dave@puseyuk.co.uk>
2024-10-21 10:16:50 +0100

    2024 20 MEXICO

--- data.js
+++ data.js
@@ -8,20 +8,20 @@ const DAYS = 24 * HOURS;
 
 var seasonYear = 2024;
 
-var roundNum = 19;
-var roundName = "United States Grand Prix";
-var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg";
+var roundNum = 20;
+var roundName = "Mexican Grand Prix";
+var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg";
 
 var sessionsData = [
 
-    // 2024-10-18 FRI
-    ["F1 Practice 1",   new Date(1729272600 * SECS),  60 * MINS],
-    ["F1 Sprint Qual",  new Date(1729287000 * SECS),  60 * MINS],
+    // 2024-10-25 FRI
+    ["F1 Practice 1",   new Date(1729881000 * SECS),  60 * MINS],
+    ["F1 Practice 2",   new Date(1729893600 * SECS),  90 * MINS],
 
-    // 2024-10-19 SAT
-    ["F1 Sprint",       new Date(1729360800 * SECS),  90 * MINS],
-    ["F1 Qualifying",   new Date(1729375200 * SECS),  60 * MINS],
+    // 2024-10-26 SAT
+    ["F1 Practice 3",   new Date(1729963800 * SECS),  60 * MINS],
+    ["F1 Qualifying",   new Date(1729976400 * SECS),  60 * MINS],
 
-    // 2024-10-20 SUN
-    ["F1 Race",         new Date(1729450800 * SECS), 180 * MINS]
+    // 2024-10-27 SUN
+    ["F1 Race",         new Date(1730059200 * SECS), 180 * MINS]
 ];
