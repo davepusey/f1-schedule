09c8e035e1a17e3ca0089f038f5ff0746627c4ff
Dave Pusey <dave@puseyuk.co.uk>
2024-11-03 12:26:50 +0000

    2024 22 LAS VEGAS

--- data.js
+++ data.js
@@ -8,20 +8,20 @@ const DAYS = 24 * HOURS;
 
 var seasonYear = 2024;
 
-var roundNum = 21;
-var roundName = "Brazilian Grand Prix";
-var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/0/05/Flag_of_Brazil.svg";
+var roundNum = 22;
+var roundName = "Las Vegas Grand Prix";
+var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg";
 
 var sessionsData = [
 
-    // 2024-11-01 FRI
-    ["F1 Practice 1",   new Date(1730471400 * SECS),  60 * MINS],
-    ["F1 Sprint Qual",  new Date(1730485800 * SECS),  60 * MINS],
+    // 2024-11-22 FRI
+    ["F1 Practice 1",   new Date(1732242600 * SECS),  60 * MINS],
+    ["F1 Practice 2",   new Date(1732255200 * SECS),  60 * MINS],
 
-    // 2024-11-02 SAT
-    ["F1 Sprint",       new Date(1730556000 * SECS),  90 * MINS],
+    // 2024-11-23 SAT
+    ["F1 Practice 3",   new Date(1732329000 * SECS),  60 * MINS],
+    ["F1 Qualifying",   new Date(1732341600 * SECS),  60 * MINS],
 
-    // 2024-11-03 SUN
-    ["F1 Qualifying",   new Date(1730629800 * SECS),  60 * MINS],
-    ["F1 Race",         new Date(1730647800 * SECS), 180 * MINS]
+    // 2024-11-24 SUN
+    ["F1 Race",         new Date(1732428000 * SECS), 180 * MINS]
 ];
