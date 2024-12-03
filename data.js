5275d827f5c03f0327ff755091d0f19f0da0c086
Dave Pusey <dave@puseyuk.co.uk>
2024-12-03 13:43:01 +0000

    2024 24 ABU DHABI

--- data.js
+++ data.js
@@ -8,28 +8,29 @@ const DAYS = 24 * HOURS;
 
 var seasonYear = 2024;
 
-var roundNum = 23;
-var roundName = "Qatar Grand Prix";
-var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/6/65/Flag_of_Qatar.svg";
+var roundNum = 24;
+var roundName = "Abu Dhabi Grand Prix";
+var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/c/cb/Flag_of_the_United_Arab_Emirates.svg";
 
 var sessionsData = [
 
-    // 2024-11-29 FRI
-    ["F1A Practice 1",  new Date(1732874400 * SECS),  40 * MINS],
-    ["F2 Practice",     new Date(1732878300 * SECS),  45 * MINS],
-    ["F1 Practice 1",   new Date(1732887000 * SECS),  60 * MINS],
-    ["F1A Practice 2",  new Date(1732892400 * SECS),  40 * MINS],
-    ["F2 Qualifying",   new Date(1732896600 * SECS),  30 * MINS],
-    ["F1 Sprint Qual",  new Date(1732901400 * SECS),  60 * MINS],
+    // 2024-12-06 FRI
+    ["F1A Practice",    new Date(1733464200 * SECS),  40 * MINS],
+    ["F2 Practice",     new Date(1733468700 * SECS),  45 * MINS],
+    ["F1 Practice 1",   new Date(1733477400 * SECS),  60 * MINS],
+    ["F2 Qualifying",   new Date(1733482800 * SECS),  30 * MINS],
+    ["F1 Practice 2",   new Date(1733490000 * SECS),  60 * MINS],
+    ["F1A Qualifying",  new Date(1733495700 * SECS),  30 * MINS],
 
-    // 2024-11-30 SAT
-    ["F1A Qualifying",  new Date(1732969200 * SECS),  30 * MINS],
-    ["F1 Sprint",       new Date(1732975200 * SECS),  90 * MINS],
-    ["F2 Race 1",       new Date(1732983600 * SECS),  45 * MINS],
-    ["F1 Qualifying",   new Date(1732989600 * SECS),  60 * MINS],
-    ["F1A Race 1",      new Date(1732997100 * SECS),  30 * MINS],
+    // 2024-12-07 SAT
+    ["F1A Race 1",      new Date(1733559300 * SECS),  30 * MINS],
+    ["F1 Practice 3",   new Date(1733567400 * SECS),  60 * MINS],
+    ["F2 Race 1",       new Date(1733573700 * SECS),  45 * MINS],
+    ["F1 Qualifying",   new Date(1733580000 * SECS),  60 * MINS],
+    ["F1A Race 2",      new Date(1733588100 * SECS),  30 * MINS],
 
-    // 2024-12-01 SUN
-    ["F2 Race 2",       new Date(1733056500 * SECS),  60 * MINS],
-    ["F1 Race",         new Date(1733068800 * SECS), 180 * MINS]
+    // 2024-12-08 SUN
+    ["F1A Race 3",      new Date(1733642100 * SECS),  30 * MINS],
+    ["F2 Race 2",       new Date(1733649900 * SECS),  60 * MINS],
+    ["F1 Race",         new Date(1733662800 * SECS), 180 * MINS]
 ];
