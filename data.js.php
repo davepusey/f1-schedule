706850f795fbd74bc0b9ef2832e3cc54285d0680
Dave Pusey <dave@puseyuk.co.uk>
2025-04-10 10:55:04 +0100

    2025 04 BAHRAIN

--- data.js.php
+++ data.js.php
@@ -12,20 +12,28 @@ const DAYS = 24 * HOURS;
 
 var seasonYear = 2025;
 
-var roundNum = 3;
-var roundName = "Japanese Grand Prix";
-var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/9/9e/Flag_of_Japan.svg";
+var roundNum = 4;
+var roundName = "Bahrain Grand Prix";
+var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/2/2c/Flag_of_Bahrain.svg";
 
 var sessionsData = [
 
-    // 2025-04-04 FRI
-    ["F1 Practice 1",   new Date(1743733800 * SECS),  60 * MINS],
-    ["F1 Practice 2",   new Date(1743746400 * SECS),  60 * MINS],
+    // 2025-04-11 FRI
+    ["F3 Practice",     new Date(1744358100 * SECS),  45 * MINS],
+    ["F2 Practice",     new Date(1744362300 * SECS),  45 * MINS],
+    ["F1 Practice 1",   new Date(1744371000 * SECS),  60 * MINS],
+    ["F3 Qualifying",   new Date(1744376400 * SECS),  30 * MINS],
+    ["F2 Qualifying",   new Date(1744379700 * SECS),  30 * MINS],
+    ["F1 Practice 2",   new Date(1744383600 * SECS),  60 * MINS],
 
-    // 2025-04-05 SAT
-    ["F1 Practice 3",   new Date(1743820200 * SECS),  60 * MINS],
-    ["F1 Qualifying",   new Date(1743832800 * SECS),  60 * MINS],
+    // 2025-04-12 SAT
+    ["F3 Race 1",       new Date(1744452900 * SECS),  40 * MINS],
+    ["F1 Practice 3",   new Date(1744461000 * SECS),  60 * MINS],
+    ["F2 Race 1",       new Date(1744467300 * SECS),  45 * MINS],
+    ["F1 Qualifying",   new Date(1744473600 * SECS),  60 * MINS],
 
-    // 2025-04-06 SUN
-    ["F1 Race",         new Date(1743915600 * SECS), 180 * MINS]
+    // 2025-04-13 SUN
+    ["F3 Race 2",       new Date(1744538100 * SECS),  45 * MINS],
+    ["F2 Race 2",       new Date(1744543500 * SECS),  60 * MINS],
+    ["F1 Race",         new Date(1744556400 * SECS), 180 * MINS]
 ];
