fb25d456717fa72faefbe1a2ccb4733c21f84a0a
Dave Pusey <dave@puseyuk.co.uk>
2025-04-18 11:15:41 +0100

    2025 05 SAUDI ARABIA

--- data.js.php
+++ data.js.php
@@ -12,28 +12,28 @@ const DAYS = 24 * HOURS;
 
 var seasonYear = 2025;
 
-var roundNum = 4;
-var roundName = "Bahrain Grand Prix";
-var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/2/2c/Flag_of_Bahrain.svg";
+var roundNum = 5;
+var roundName = "Saudi Arabian Grand Prix";
+var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/0/0d/Flag_of_Saudi_Arabia.svg";
 
 var sessionsData = [
 
-    // 2025-04-11 FRI
-    ["F3 Practice",     new Date(1744358100 * SECS),  45 * MINS],
-    ["F2 Practice",     new Date(1744362300 * SECS),  45 * MINS],
-    ["F1 Practice 1",   new Date(1744371000 * SECS),  60 * MINS],
-    ["F3 Qualifying",   new Date(1744376400 * SECS),  30 * MINS],
-    ["F2 Qualifying",   new Date(1744379700 * SECS),  30 * MINS],
-    ["F1 Practice 2",   new Date(1744383600 * SECS),  60 * MINS],
+    // 2025-04-18 FRI
+    ["F2 Practice",     new Date(1744969500 * SECS),  45 * MINS],
+    ["F1A Practice",    new Date(1744974300 * SECS),  40 * MINS],
+    ["F1 Practice 1",   new Date(1744983000 * SECS),  60 * MINS],
+    ["F2 Qualifying",   new Date(1744988400 * SECS),  30 * MINS],
+    ["F1 Practice 2",   new Date(1744995600 * SECS),  60 * MINS],
+    ["F1A Qualifying",  new Date(1745001000 * SECS),  30 * MINS],
 
-    // 2025-04-12 SAT
-    ["F3 Race 1",       new Date(1744452900 * SECS),  40 * MINS],
-    ["F1 Practice 3",   new Date(1744461000 * SECS),  60 * MINS],
-    ["F2 Race 1",       new Date(1744467300 * SECS),  45 * MINS],
-    ["F1 Qualifying",   new Date(1744473600 * SECS),  60 * MINS],
+    // 2025-04-19 SAT
+    ["F1A Race 1",      new Date(1745065200 * SECS),  30 * MINS],
+    ["F1 Practice 3",   new Date(1745069400 * SECS),  60 * MINS],
+    ["F2 Race 1",       new Date(1745075700 * SECS),  45 * MINS],
+    ["F1 Qualifying",   new Date(1745082000 * SECS),  60 * MINS],
 
-    // 2025-04-13 SUN
-    ["F3 Race 2",       new Date(1744538100 * SECS),  45 * MINS],
-    ["F2 Race 2",       new Date(1744543500 * SECS),  60 * MINS],
-    ["F1 Race",         new Date(1744556400 * SECS), 180 * MINS]
+    // 2025-04-20 SUN
+    ["F1A Race 2",      new Date(1745150700 * SECS),  30 * MINS],
+    ["F2 Race 2",       new Date(1745155500 * SECS),  60 * MINS],
+    ["F1 Race",         new Date(1745168400 * SECS), 180 * MINS]
 ];
