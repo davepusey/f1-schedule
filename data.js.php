b990dd6c417d0ff254320479a1d9ea113d3f5a01
Dave Pusey <dave@puseyuk.co.uk>
2025-09-24 13:34:59 +0100

    2025 18 SINGAPORE

--- data.js.php
+++ data.js.php
@@ -12,24 +12,24 @@ const DAYS = 24 * HOURS;
 
 var seasonYear = 2025;
 
-var roundNum = 17;
-var roundName = "Azerbaijan Grand Prix";
-var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/d/dd/Flag_of_Azerbaijan.svg";
+var roundNum = 18;
+var roundName = "Singapore Grand Prix";
+var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg";
 
 var sessionsData = [
 
-    // 2025-09-19 FRI
-    ["F2 Practice",     new Date(1758261600 * SECS),  45 * MINS],
-    ["F1 Practice 1",   new Date(1758270600 * SECS),  60 * MINS],
-    ["F2 Qualifying",   new Date(1758276000 * SECS),  30 * MINS],
-    ["F1 Practice 2",   new Date(1758283200 * SECS),  60 * MINS],
+    // 2025-10-03 FRI
+    ["F1A Practice",    new Date(1759475400 * SECS),  40 * MINS],
+    ["F1 Practice 1",   new Date(1759483800 * SECS),  60 * MINS],
+    ["F1A Qualifying",  new Date(1759489200 * SECS),  30 * MINS],
+    ["F1 Practice 2",   new Date(1759496400 * SECS),  60 * MINS],
 
-    // 2025-09-20 SAT
-    ["F1 Practice 3",   new Date(1758357000 * SECS),  60 * MINS],
-    ["F2 Race 1",       new Date(1758363300 * SECS),  45 * MINS],
-    ["F1 Qualifying",   new Date(1758369600 * SECS),  60 * MINS],
+    // 2025-10-04 SAT
+    ["F1A Race 1",      new Date(1759561200 * SECS),  30 * MINS],
+    ["F1 Practice 3",   new Date(1759570200 * SECS),  60 * MINS],
+    ["F1 Qualifying",   new Date(1759582800 * SECS),  60 * MINS],
 
-    // 2025-09-21 SUN
-    ["F2 Race 2",       new Date(1758438000 * SECS),  60 * MINS],
-    ["F1 Race",         new Date(1758452400 * SECS), 180 * MINS]
+    // 2025-10-05 SUN
+    ["F1A Race 2",      new Date(1759649100 * SECS),  30 * MINS],
+    ["F1 Race",         new Date(1759665600 * SECS), 180 * MINS]
 ];
