662ff5f5d2f746bd05731edd5c49b4b1d74c1bc4
Dave Pusey <dave@puseyuk.co.uk>
2025-12-09 17:55:53 +0000

    2026 00 TESTING

--- data.js.php
+++ data.js.php
@@ -10,27 +10,61 @@ const DAYS = 24 * HOURS;
 
 //----------------------------------------------------------------------------------------------------------------------
 
-var seasonYear = 2025;
+var seasonYear = 2026;
 
-var roundNum = 24;
-var roundName = "Abu Dhabi Grand Prix";
-var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/c/cb/Flag_of_the_United_Arab_Emirates.svg";
+var roundNum = 0;
+var roundName = "Pre-Season Testing";
+var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/0/0d/F1_%28registered_trademark%29.svg";
 
 var sessionsData = [
 
-    // 2025-12-05 FRI
-    ["F2 Practice",     new Date(1764918300 * SECS),  45 * MINS],
-    ["F1 Practice 1",   new Date(1764927000 * SECS),  60 * MINS],
-    ["F2 Qualifying",   new Date(1764932400 * SECS),  30 * MINS],
-    ["F1 Practice 2",   new Date(1764939600 * SECS),  60 * MINS],
+    // TEST 1
 
-    // 2025-12-06 SAT
-    ["F1 Practice 3",   new Date(1765017000 * SECS),  60 * MINS],
-    ["F2 Race 1",       new Date(1765023300 * SECS),  45 * MINS],
-    ["F1 Qualifying",   new Date(1765029600 * SECS),  60 * MINS],
-    ["FE Sao Paulo",    new Date(1765040700 * SECS),  45 * MINS],
+    // 2025-01-26 MON
+    ["T1 Day 1 AM",        new Date(Date.UTC(2026, 0, 26, 7, 0, 0)), 240 * MINS],
+    ["T1 Day 1 PM",        new Date(Date.UTC(2026, 0, 26, 12, 0, 0)), 240 * MINS],
 
-    // 2025-12-07 SUN
-    ["F2 Race 2",       new Date(1765098900 * SECS),  60 * MINS],
-    ["F1 Race",         new Date(1765112400 * SECS), 180 * MINS]
+    // 2025-01-27 TUE
+    ["T1 Day 2 AM",        new Date(Date.UTC(2026, 0, 27, 7, 0, 0)), 240 * MINS],
+    ["T1 Day 2 PM",        new Date(Date.UTC(2026, 0, 27, 12, 0, 0)), 240 * MINS],
+
+    // 2025-01-28 WED
+    ["T1 Day 3 AM",        new Date(Date.UTC(2026, 0, 28, 7, 0, 0)), 240 * MINS],
+    ["T1 Day 3 PM",        new Date(Date.UTC(2026, 0, 28, 12, 0, 0)), 240 * MINS],
+
+    // 2025-01-29 THU
+    ["T1 Day 4 AM",        new Date(Date.UTC(2026, 0, 29, 7, 0, 0)), 240 * MINS],
+    ["T1 Day 4 PM",        new Date(Date.UTC(2026, 0, 29, 12, 0, 0)), 240 * MINS],
+
+    // 2025-01-30 FRI
+    ["T1 Day 5 AM",        new Date(Date.UTC(2026, 0, 30, 7, 0, 0)), 240 * MINS],
+    ["T1 Day 5 PM",        new Date(Date.UTC(2026, 0, 30, 12, 0, 0)), 240 * MINS],
+
+    // TEST 2
+
+    // 2025-02-11 WED
+    ["T2 Day 1 AM",        new Date(Date.UTC(2026, 1, 11, 7, 0, 0)), 240 * MINS],
+    ["T2 Day 1 PM",        new Date(Date.UTC(2026, 1, 11, 12, 0, 0)), 240 * MINS],
+
+    // 2025-02-12 THU
+    ["T2 Day 2 AM",        new Date(Date.UTC(2026, 1, 12, 7, 0, 0)), 240 * MINS],
+    ["T2 Day 2 PM",        new Date(Date.UTC(2026, 1, 12, 12, 0, 0)), 240 * MINS],
+
+    // 2025-02-13 FRI
+    ["T2 Day 3 AM",        new Date(Date.UTC(2026, 1, 13, 7, 0, 0)), 240 * MINS],
+    ["T2 Day 3 PM",        new Date(Date.UTC(2026, 1, 13, 12, 0, 0)), 240 * MINS],
+
+    // TEST 3
+
+    // 2025-02-18 WED
+    ["T3 Day 1 AM",        new Date(Date.UTC(2026, 1, 18, 7, 0, 0)), 240 * MINS],
+    ["T3 Day 1 PM",        new Date(Date.UTC(2026, 1, 18, 12, 0, 0)), 240 * MINS],
+
+    // 2025-02-19 THU
+    ["T3 Day 2 AM",        new Date(Date.UTC(2026, 1, 19, 7, 0, 0)), 240 * MINS],
+    ["T3 Day 2 PM",        new Date(Date.UTC(2026, 1, 19, 12, 0, 0)), 240 * MINS],
+
+    // 2025-02-20 FRI
+    ["T3 Day 3 AM",        new Date(Date.UTC(2026, 1, 20, 7, 0, 0)), 240 * MINS],
+    ["T3 Day 3 PM",        new Date(Date.UTC(2026, 1, 20, 12, 0, 0)), 240 * MINS]
 ];
