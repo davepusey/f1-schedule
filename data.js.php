a921c6ec9bae39045e5dc71c35b949f4fa5d6ae2
Dave Pusey <dave@puseyuk.co.uk>
2026-04-02 21:39:13 +0100

    2026 04 MIAMI

--- data.js.php
+++ data.js.php
@@ -12,20 +12,23 @@ const DAYS = 24 * HOURS;
 
 var seasonYear = 2026;
 
-var roundNum = 3;
-var roundName = "Japanese Grand Prix";
-var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/9/9e/Flag_of_Japan.svg";
+var roundNum = 4;
+var roundName = "Miami Grand Prix";
+var roundFlag = "https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg";
 
 var sessionsData = [
 
-    // 2026-03-27 FRI
-    ["F1 Practice 1",   new Date(1774578600 * SECS),  60 * MINS],
-    ["F1 Practice 2",   new Date(1774591200 * SECS),  60 * MINS],
+    // 2026-05-01 FRI
+    ["F2 Practice 1",   new Date(1777644000 * SECS),  45 * MINS],
+    ["F1 Practice 1",   new Date(1777653000 * SECS),  60 * MINS],
+    ["F1 Sprint Qual",  new Date(1777667400 * SECS),  60 * MINS],
 
-    // 2026-03-28 SAT
-    ["F1 Practice 3",   new Date(1774665000 * SECS),  60 * MINS],
-    ["F1 Qualifying",   new Date(1774677600 * SECS),  60 * MINS],
+    // 2026-05-02 SAT
+    ["F2 Race 1",       new Date(1777730400 * SECS),  45 * MINS],
+    ["F1 Sprint",       new Date(1777737600 * SECS),  90 * MINS],
+    ["F1 Qualifying",   new Date(1777752000 * SECS),  60 * MINS],
 
-    // 2026-03-29 SUN
-    ["F1 Race",         new Date(1774760400 * SECS), 180 * MINS]
+    // 2026-05-03 SUN
+    ["F2 Race 2",       new Date(1777825800 * SECS),  60 * MINS],
+    ["F1 Race",         new Date(1777838400 * SECS), 180 * MINS]
 ];
